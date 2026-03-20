d1983ba2e7b04ef4939d3f50a80e1f37cd9abf59
iMax-git <latelierroleplay@gmail.com>
2026-03-20 12:48:14 +0100

    Ajout de la base payment service.

--- payment-service/src/test/java/fr/imax/paymentservice/PaymentServiceApplicationTests.java
+++ payment-service/src/test/java/fr/imax/paymentservice/PaymentServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package eventdriven.kafka.emailservice;
+package fr.imax.paymentservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EmailServiceApplicationTests {
+class PaymentServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
